1caea147894867eab5a53e9efe73b47de67979fa
chenh <chenhang@oppo.com>
2018-03-19 16:39:22 +0800

    增加文档

--- src/utils/assist.js
+++ src/utils/assist.js
@@ -7,3 +7,17 @@ export function oneOf (value, validList) {
   }
   return false
 }
+
+
+export function consoleOutput (e, t) {
+  if (typeof console.groupCollapsed === 'fcuntion') {
+    console.groupCollapsed(t)
+    console.log(e)
+    console.groupEnd()
+  } else {
+    console.log(t)
+    console.log('-----------')
+    console.log(e)
+    console.log('-----------')
+  }
+}
--- test/unit/specs/assist.spec.js
+++ test/unit/specs/assist.spec.js
@@ -1,4 +1,4 @@
-import { oneOf } from '@/utils/assist'
+import { oneOf, consoleOutput } from '@/utils/assist'
 
 describe('oneOf', () => {
   it('should return true', () => {
@@ -7,5 +7,17 @@ describe('oneOf', () => {
 
   it('should return false', () => {
     expect(oneOf('a', ['1', 'b', 'adc'])).to.equal(false)
+  })
+})
+
+
+describe('consoleOutput', () => {
+  it('should return sss', () => {
+    console.log("-----------");
+    consoleOutput(["", "掘金餐厅招前端厨师：", "", "地处望京，厨具先进，食材鲜美，朝十晚七，周六日均不营业。", "", 
+    "招牌菜有爆炒 Vue.js、红烧 Node.js、Chrome 炖 Extension、宫爆 Panfish……", "", 
+    "要求注意卫生，以做菜为乐，有精进厨艺之心；熟悉烹饪基础理论者优先，可开发新式菜肴者优先。", "", "简历注明「FROM_JUEJIN_CONSOLE」投至：webuster@xitu.io。"].join("\n"), 
+    "BUG 为何迟迟不改？页面为何加载失败？元素为何难以对齐？这一切究竟是人性的扭曲还是道德的沦丧？");
+    console.log("-----------")
   })
 })
